791e02015d1f4e050d3f4883115b82711330792a
Md. Tanvir Arif Siddiqui <tanvirtmm@gmail.com>
2023-02-03 15:57:01 +0600

    All Files have Uploaded
    HTML CSS and Javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.ceil(Math.random() * 6);
+var randomNumber2 = Math.ceil(Math.random() * 6);
+// console.log(randomNumber1);
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML=" 🚩 Player 1 Wins!";
+} 
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩 ";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
